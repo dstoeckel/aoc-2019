23678097d423a5297dd05ccc8f6b201c626b5206
Daniel Stöckel <daniel@somweyr.de>
2019-12-06 15:26:44 +0100

    Port 02-intcode to the intcode interpreter

--- src/02-intcode.rs
+++ src/02-intcode.rs
@@ -1,36 +1,6 @@
 use std::io::Read;
 
-#[test]
-fn test_examples() {
-    let instr = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
-
-    assert_eq!(3500, evaluate(instr));
-}
-
-fn evaluate(mut instructions: Vec<usize>) -> usize {
-    let mut i = 0;
-    while i < instructions.len() {
-        let opcode = instructions[i];
-
-        if opcode == 99 {
-            break;
-        }
-
-        let s1 = instructions[instructions[i + 1]];
-        let s2 = instructions[instructions[i + 2]];
-        let t = instructions[i + 3];
-
-        instructions[t] = match opcode {
-            1 => s1 + s2,
-            2 => s1 * s2,
-            o => panic!("Unhandled opcode {}", o),
-        };
-
-        i += 4;
-    }
-
-    instructions[0]
-}
+mod intcode;
 
 fn main() {
     let path = std::env::args()
@@ -46,7 +16,7 @@ fn main() {
     let instructions = code
         .trim()
         .split(',')
-        .map(|x| str::parse::<usize>(x).unwrap())
+        .map(|x| str::parse::<isize>(x).unwrap())
         .collect::<Vec<_>>();
 
     for noun in 0..99 {
@@ -56,7 +26,7 @@ fn main() {
             memory[1] = noun;
             memory[2] = verb;
 
-            if evaluate(memory) == 19690720 {
+            if intcode::evaluate(memory) == 19690720 {
                 println!(
                     "Found noun = {} and verb = {}. Combined output is {}",
                     noun,
